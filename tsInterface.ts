d6cbf59f7ede1873612e1447779ae923cbe49cec
Ayman Cassim <ayman.cassim@bdo.mu>
2026-05-05 15:34:07 +0400

    Testing merge conflict

--- tsInterface.ts
+++ tsInterface.ts
@@ -17,7 +17,7 @@ function getFirstElement<T>(arr: T[]): T | undefined {
 // Some test data to see the test the output of the function and see if the program works well
 const sampleProducts: Product[] = [
     { id: 1, name: 'Widget', price: 9.99, description: 'Small widget' },
-    { id: 2, name: 'Gadget', price: 19.95 },
+    { id: 2, name: 'Gadget', price: 19.90 },
     { id: 3, name: 'Thingamajig', price: 4.50, description: 'Useful item' },
 ];
 
